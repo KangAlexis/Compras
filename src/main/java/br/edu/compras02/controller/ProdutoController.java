2f8aa7d68321c4fcf40ab0b10607da62c0ffafc8
Alexis <Alexis@DESKTOP-2TFC81Q>
2025-06-27 01:21:48 -0300

    Projeto Atualizado

---
+++ src/main/java/br/edu/compras02/controller/ProdutoController.java
@@ -0,0 +1,46 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.edu.compras02.controller;
+
+import java.util.ArrayList;
+import br.edu.compras02.model.Produto;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Alexis
+ */
+public class ProdutoController {
+    private ArrayList<Produto> listaDeProdutos = new ArrayList<>();
+
+    public void adicionarProduto(Produto produto) {
+        listaDeProdutos.add(produto);
+    }
+
+    public void editarProduto(int index, Produto produto) {
+        if (index >= 0 && index < listaDeProdutos.size()) {
+            listaDeProdutos.set(index, produto);
+        }
+    }
+
+    public void excluirProduto(int index) {
+        if (index >= 0 && index < listaDeProdutos.size()) {
+            listaDeProdutos.remove(index);
+        }else{
+            JOptionPane.showMessageDialog(null, "Nenhum produto selecionado para exclusão", "Erro ao excluir produto", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+
+    public ArrayList<Produto> getListaDeProdutos() {
+        return listaDeProdutos;
+    }
+
+    public Produto getProduto(int index) {
+        if (index >= 0 && index < listaDeProdutos.size()) {
+            return listaDeProdutos.get(index);
+        }
+        return null;
+    }
+}
--- src/main/java/br/edu/compras02/model/Produto.java
+++ src/main/java/br/edu/compras02/model/Produto.java
@@ -5,5 +5,55 @@
 package br.edu.compras02.model;
 
 public class Produto {
-    
+    private String nome;
+    private int quantidade;
+    private double preco;
+    private String codigo;
+
+    public Produto() {
+    }
+
+    public Produto(String nome, int quantidade, double preco, String codigo) {
+        this.nome = nome;
+        this.quantidade = quantidade;
+        this.preco = preco;
+        this.codigo = codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getQuantidade() {
+        return quantidade;
+    }
+
+    public void setQuantidade(int quantidade) {
+        this.quantidade = quantidade;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+
+    @Override
+    public String toString() {
+        return "Produto{" + "nome=" + nome + ", quantidade=" + quantidade + ", preco=" + preco + ", codigo=" + codigo + '}';
+    }
 }
